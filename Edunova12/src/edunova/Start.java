31a40ea912da8e6d1474bdc77e4bb833db5b13d9
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-07-29 15:29:28 +0100

    Edunova12

--- Edunova12/src/edunova/Start.java
+++ Edunova12/src/edunova/Start.java
@@ -1,11 +1,34 @@
 package edunova;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Start {
 	
-	public Start() {
+	private List<Smjer> smjerovi;
 	
+	public Start() {
+		smjerovi = new ArrayList<Smjer>();
+		glavniIzbornik();
 	}
 	
+	private void glavniIzbornik() {
+		System.out.println("**** Edunova APP konzola ****");
+		System.out.println(" IZBORNIK ");
+		System.out.println(" 1. Smjerovi");
+		System.out.println(" 2. Polaznici");
+		System.out.println(" 3. Predavači");
+		System.out.println(" 4. Grupe");
+		System.out.println(" 5. Izlazi iz programa");
+		ucitajGlavnuAkciju();
+		
+	}
+
+	private void ucitajGlavnuAkciju() {
+		
+		
+	}
+
 	public static void main(String[] args) {
 		new Start();
 	}
---
+++ Edunova12/src/edunova/UIaz.java
@@ -0,0 +1,30 @@
+package edunova;
+
+import java.util.Scanner;
+
+public class UIaz {
+	
+	public static int ucitajInt(
+			String poruka, 
+			String greska, 
+			int min,
+			int max) {
+		int broj=0;
+		Scanner ulaz = new Scanner(System.in);
+		while(true) {
+			System.out.println(poruka);
+			try {
+				broj = Integer.parseInt(ulaz.nextLine());
+				if (broj<min || broj>max) {
+					continue;
+				}
+				break;
+			} catch (Exception e) {
+				System.out.println(greska);
+			}
+		}
+		ulaz.close();
+		return broj;
+	}
+
+}
