0002edcbe5fef957a607d4e77c0efee1cf8fcbb5
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-08-03 10:44:45 +0100

    vjezba

--- ConsoleAppPractice/src/aplikacija/Start.java
+++ ConsoleAppPractice/src/aplikacija/Start.java
@@ -55,14 +55,70 @@ public class Start {
 		switch(Ulaz.ucitajInt("Odaberite akciju: ", 
 				"Niste unijeli cijeli broj", 1, 5)) {
 		case 1 -> korisnikPregled();
+		case 2 -> korisnikUnosNovog();
+		case 3 -> korisnikPromjena();
+		case 4 -> korisnikBrisanje();
 		case 5 -> glavniIzbornik();
 		}
 		
 	}
 
-	private Object korisnikPregled() {
-		// TODO Auto-generated method stub
-		return null;
+	private void korisnikBrisanje() {
+		korisnikStavke("Trenutno dostupno u aplikaciji");
+		int redniBroj = Ulaz.ucitajInt("Odaberite redni broj za brisanje: ", 
+				"Niste unijeli cijeli broj", 1, korisnici.size());
+		korisnici.remove(redniBroj-1);
+		korisnikIzbornik();
+	}
+
+	private void korisnikPromjena() {
+		korisnikStavke("Trenutno dostupno u aplikaciji");
+		int redniBroj = Ulaz.ucitajInt("Odaberite redni broj za promjenu: ", 
+				"Niste unijeli cijeli broj", 1, korisnici.size());
+		Korisnik korisnikZaPromjenu = korisnici.get(redniBroj-1);
+		korisnikZaPromjenu = korisnikPostaviVrijednosti(korisnikZaPromjenu);
+		korisnici.set(redniBroj-1, korisnikZaPromjenu);
+		korisnikIzbornik();
+	}
+
+	private void korisnikUnosNovog() {
+		Korisnik k = new Korisnik();
+		k = korisnikPostaviVrijednosti(k);
+		korisnici.add(k);
+		korisnikIzbornik();
+	}
+
+	private Korisnik korisnikPostaviVrijednosti(Korisnik k) {
+		k.setSifra(Ulaz.ucitajInt("Unesite sifru: ",
+    			"sifra mora biti cijeli broj",
+    			1, Integer.MAX_VALUE));
+    	k.setIme(Ulaz.ucitajString("Unesi ime korisnika: ",
+    			"Ime obavezno"));
+    	k.setPrezime(Ulaz.ucitajString("Unesi prezime korisnika: ",
+    			"Prezime obavezno"));
+    	k.setEmail(Ulaz.ucitajString("Unesi email korisnika: ",
+    			"Email obavezan"));
+    	return k;
+	}
+
+	private void korisnikPregled() {
+		korisnikStavke("Pregled unesenih smjerova");
+		korisnikIzbornik();
+	}
+
+	private void korisnikStavke(String naslov) {
+		System.out.println(naslov);
+		System.out.println("--------------------");
+		if(korisnici.size()==0) {
+			System.out.println("Nema unesenih korisnika");
+		}else {
+			Korisnik k;
+			for(int i=0;i<korisnici.size();i++) {
+				k= korisnici.get(i);
+				System.out.println((i + 1) + ". " + k.getIme() + 
+						" " + k.getPrezime());
+			}	
+		}
 	}
 
 	public static void main(String[] args) {
--- ConsoleAppPractice/src/aplikacija/Ulaz.java
+++ ConsoleAppPractice/src/aplikacija/Ulaz.java
@@ -1,8 +1,12 @@
 package aplikacija;
 
+
 import java.util.Scanner;
 
+
 public class Ulaz {
+	
+	public static Scanner scanner;
 	
 	public static int ucitajInt(
 			String poruka, 
@@ -10,23 +14,86 @@ public class Ulaz {
 			int min, 
 			int max) {
 		int broj=0;
-		Scanner ulaz = new Scanner(System.in);
 		while(true) {
 			System.out.print(poruka);
 			try {
-				broj = Integer.parseInt(ulaz.nextLine());
-				if (broj<min  || broj>max) {
+				
+				broj = Integer.parseInt(
+						scanner.nextLine());
+				if (broj<min || broj>max) {
 					System.out.println("Broj ne smije biti manji od " + min
-							+ " ili veci od " + max);
+							+ " ili veći od " + max);
 					continue;
 				}
 				break;
 			} catch (Exception e) {
+				//e.printStackTrace();
 				System.out.println(greska);
 			}
 		}
-		ulaz.close();
+		
 		return broj;
 	}
-
+	
+	public static String ucitajString(String poruka,String greska) {
+		String s="";
+		while(true) {
+			System.out.print(poruka);
+			s = scanner.nextLine();
+			if(s.trim().equals("")){
+				System.out.println(greska);
+				continue;
+			}
+			break;
+		}
+		
+		return s;
+	}
+	
+	
+	
+	public static double ucitajDouble(
+			String poruka, 
+			String greska, 
+			double min, 
+			double max) {
+		double broj=0;
+		while(true) {
+			System.out.print(poruka);
+			try {
+				
+				broj = Double.parseDouble(
+						// Zamjeniti s Scanner
+						scanner.nextLine());
+				if (broj<min || broj>max) {
+					System.out.println("Broj ne smije biti manji od " + min
+							+ " ili veći od " + max);
+					continue;
+				}
+				break;
+			} catch (Exception e) {
+				//e.printStackTrace();
+				System.out.println(greska);
+			}
+		}
+		
+		return broj;
+	}
+	
+	public static boolean ucitajBoolean(String poruka, String greska) {
+		String s;
+		while(true) {
+			System.out.print(poruka);
+			s=scanner.nextLine();
+			if(s.trim().toLowerCase().equals("da")) {
+				return true;
+			}
+			if(s.trim().toLowerCase().equals("ne")) {
+				return false;
+			}
+			System.out.println(greska + " (unos da/ne)");
+		}
+	}
+	
+	
 }
