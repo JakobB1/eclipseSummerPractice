db0c9f4d9700c96f94731d11acb0a0abf641a5dd
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-08-04 12:02:31 +0100

    vjezba

---
+++ ConsoleAppPractice/src/aplikacija/Racun.java
@@ -0,0 +1,14 @@
+package aplikacija;
+
+import java.util.Date;
+
+public class Racun {
+	
+	private int sifra;
+	private Date datumpocetka;
+	private String brojracuna;
+	private double cijena;
+	private Korisnik korisnici;
+	private Djelatnik djelatnici;
+
+}
