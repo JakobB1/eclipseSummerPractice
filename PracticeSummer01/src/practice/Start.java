5c2e8530b9be519f011852f48ead758d6924989e
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-08-07 15:28:41 +0100

    vjezba

--- PracticeSummer01/src/practice/Start.java
+++ PracticeSummer01/src/practice/Start.java
@@ -1,11 +1,27 @@
 package practice;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Start {
 	
+	private List<Korisnik> korisnici;	
+	
 	public Start() {
-		
+		korisnici = new ArrayList<Korisnik>();
+		glavniIzbornik();
 	}
 	
+	private void glavniIzbornik() {
+		 System.out.println("***** PC SHOP Vjezba konzola");
+		 System.out.println(" IZBORNIK ");
+		 System.out.println(" 1. Korisnici");
+		 System.out.println(" 2. Djelatnici");
+		 System.out.println(" 3. Racuni");
+		 System.out.println(" 4. Proizvodi");
+		 System.out.println(" 5. Izlaz iz programa");
+	}
+
 	public static void main(String[] args) {
 		new Start();
 	}
